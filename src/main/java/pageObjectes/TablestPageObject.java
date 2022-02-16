47b130e0123b8bc5b8e933a14f420be679effe1a
Haroon Aimal <m.haroon.emran2016@gmail.com>
2022-02-16 14:15:29 -0500

    New Update kk

--- src/main/java/pageObjectes/TablestPageObject.java
+++ src/main/java/pageObjectes/TablestPageObject.java
@@ -8,13 +8,18 @@ import core.baseC;
 
 public class TablestPageObject extends baseC {
 	
+	//-----------------------------------------------------------------
+	//-----------------------------------------------------------------
 	public TablestPageObject()
 	{
 		PageFactory.initElements(driver, this);
 	}
-
+	
 	@FindBy(xpath = "//a[contains(text(),'Samsung Galaxy Tab 10.1')]")
 	private WebElement SamsungGalaxyTab;
+	
+	//-----------------------------------------------------------------
+	//-----------------------------------------------------------------
 	
 	
 }
